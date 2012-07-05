4d3bc694cf26677c0d75b2199ff8ad294b2453ff
Scott J. Krieder <skrieder@iit.edu>
2012-07-05 13:44:19 -0500

    updated benchmark files and logs

--- benchMatMul.sh
+++ benchMatMul.sh
@@ -1,12 +1,12 @@
 #! /bin/bash                                                                                                                                                                       
 throttle=1
 jobs=1
-# 1 through 5 for the throttles
-for i in {1..10}
+# do the overall test, this many times
+for i in {1..9}
 do
-matrixSize=32
+matrixSize=1376
     # 1 through 8 for the sleeptimes
-    for k in {1..12}
+    for k in {1..10}
     do
 	echo ""
 	echo ""
@@ -15,12 +15,12 @@ matrixSize=32
 	echo "The matrix size is   : " $matrixSize >> logs/log$i.txt
 
 	# generate the matrix
-	./gen $matrixSize
+	./gen $matrixSize Inputs/matrixIn0.txt
 
 	# run the matrix multiply
 	(time ./run $throttle $jobs) 2>> logs/log$i.txt
 
 	# double the matrix size
-	matrixSize=$(($matrixSize+$matrixSize))    
+	matrixSize=$(($matrixSize+1376))    
     done
 done

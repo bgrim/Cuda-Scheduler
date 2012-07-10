843f3592cc7bde542cdca61a132393319a7be3bd
Scott J. Krieder <skrieder@iit.edu>
2012-07-10 12:00:34 -0500

    added a lower bar bench for sequential matrix

--- benchMatMul.sh
+++ benchMatMul.sh
@@ -1,5 +1,5 @@
 #! /bin/bash             
-throttle=2
+throttle=1
 jobs=32
 # do the overall test, this many times
 for i in {1..1}
@@ -9,16 +9,10 @@ matrixSize=32
     # 1 through 8 for the sleeptimes
     for k in {1..42}
     do
-#	echo ""
-#	echo ""
-#	echo "Throttle count equals: " $throttle >> logs/log$i.txt
-#	echo "The number of jobs is: " $jobs >> logs/log$i.txt
-#	echo "The matrix size is   : " $matrixSize >> logs/log$i.txt
-
 	# generate the matrix
         for((c=0;c<=$jobs;c++))
         do
-	  ./gen $matrixSize Inputs/matrixIn$c.txt
+	  ./genMatrix $matrixSize Inputs/matrixIn$c.txt
         done
 
 	# run the matrix multiply
@@ -31,16 +25,11 @@ matrixSize=1376
     # 1 through 8 for the sleeptimes
     for k in {1..11}
     do
-#	echo ""
-#	echo ""
-#	echo "Throttle count equals: " $throttle >> logs/log$i.txt
-#	echo "The number of jobs is: " $jobs >> logs/log$i.txt
-#	echo "The matrix size is   : " $matrixSize >> logs/log$i.txt
 
 	# generate the matrix
         for((c=0;c<=$jobs;c++))
         do
-	  ./gen $matrixSize Inputs/matrixIn$c.txt
+	  ./genMatrix $matrixSize Inputs/matrixIn$c.txt
         done
 
 	# run the matrix multiply
--- matrixMul_kernel.cu
+++ matrixMul_kernel.cu
@@ -1,3 +1,6 @@
+#include <stdio.h>
+#include <stdlib.h>
+
 //declare globals and structs and headers
 struct timeval tp;
 double getTime_sec();
